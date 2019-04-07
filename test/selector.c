1f9504ddd63b80ba9b76fe492cff8efc07a32742
Daeeun Kim <eodmsabc@gmail.com>
2019-04-07 17:55:46 +0900

    selector and trial

--- test/selector.c
+++ test/selector.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <linux/rotation.h>
 
-int main()
+int main(int argc, char** argv)
 {
+    int input;
+    long sysret;
+
+    FILE *intfile;
+
+    if (argc != 2) // Check argument
+    {
+        fprintf(stderr, "selector: 1 integer argument is required.\n");
+        return 1;
+    }
+
+    input = atoi(argv[1]);
+
+    while (1) {
+        if (rotlock_write(90, 90))
+        {
+            fprintf(stderr, "selector: Failed to request write lock.\n");
+            return 1;
+        }
+
+        intfile = fopen("integer", "w");
+        fprintf(intfile, "%d", input);
+        fclose(intfile);
+
+        printf("selector: %d\n", input++);
+
+        if (rotunlock_write(90, 90))
+        {
+            fprintf(stderr, "selector: Failed to release write lock.\n");
+            return 1;
+        }
+    }
+
     return 0;
 }
--- test/trial.c
+++ test/trial.c
@@ -1,6 +1,66 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <linux/rotation.h>
 
-int main()
+int file_exist (char *filename)
 {
+    struct stat buffer;
+    return (stat (filename, &buffer) == 0);
+}
+
+void prime_factorization(int id, int num)
+{
+    printf("trial-%d: %d = ", id, num);
+
+    for (int div = 2; div < num; div++)
+    {
+        if (num % div == 0)
+        {
+            num /= div;
+            printf("%d * ", div);
+            div--;
+            continue;
+        }
+    }
+    printf("%d\n", num);
+}
+
+int main(int argc, char** argv)
+{
+    int id;
+    int num;
+    
+    FILE *intfile;
+
+    if (argc != 2) // Check argument
+    {
+        fprintf(stderr, "trial: 1 integer argument is required.\n");
+        return 1;
+    }
+
+    id = atoi(argv[1]);
+
+    while (1) {
+        if (rotlock_read(90, 90))
+        {
+            fprintf(stderr, "trial-%d: Failed to request read lock.\n", id);
+            return 1;
+        }
+
+        if (file_exist("integer"))
+        {
+            intfile = fopen("integer", "r");
+            fscanf(intfile, "%d", &num);
+            fclose(intfile);
+
+            prime_factorization(id, num);
+        }
+
+        if (rotunlock_read(90, 90))
+        {
+            fprintf(stderr, "trial-%d: Failed to release read lock.\n", id);
+            return 1;
+        }
+    }
+
     return 0;
 }
