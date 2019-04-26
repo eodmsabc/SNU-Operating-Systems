8444c41a4d1d3574f74d3ca7be48d61f62e59956
Daeeun Kim <eodmsabc@gmail.com>
2019-04-27 01:01:06 +0900

    proj3 initial

---
+++ file_to_sdcard.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "this script mount, copy file and unmount automatecally."
+echo "this script needs sudo, so you should execute this script with sudo."
+
+if [[ -n "$1" ]]
+then
+    echo "copy $1"
+else
+    echo "usage : $0 <fileLocation> <devicelocation> <mountlocation>"
+    echo "script terminate"
+    exit 1
+fi
+
+if [[ -n "$2" ]]
+then
+    echo "to $2 device"
+else
+    echo "usage : $0 <fileLocation> <devicelocation> <mountlocation>"
+    echo "script terminate"
+    exit 1
+fi
+
+if [[ -n "$3" ]]
+then
+    echo "with $3 mount position"
+else
+    echo "usage : $0 <fileLocation> <devicelocation> <mountlocation>"
+    echo "script terminate"
+    exit 1
+fi
+
+
+copypos="$3"
+copypos+='/root'
+
+echo "mounting.."
+mount "$2" "$3" || { echo "mount failure!"; exit 1; }
+echo "copy file to sdcard"
+cp "$1" "$copypos" || { echo "copy failed!"; exit 1; }
+echo "unmounting.."
+umount "$3" || { echo "umount failure!"; exit 1; }
+
+echo "script end"
+exit 0
+
