42b46999e6f87279ca3d71f4b28f14033cc3e00f
Jeong-ho Sohn <nhosj2@gmail.com>
2019-06-01 01:00:10 +0900

    file_loc spec fulfilled
    google maps link and pretty outputs

--- test/file_loc.c
+++ test/file_loc.c
@@ -5,15 +5,27 @@
 int main(int argc, char* argv[])
 {
     struct gps_location gps;
+
+    if (argc != 2) {
+        printf("this utility takes exactly one argument: [filepath]\n");
+        return -1;
+    }
     if (syscall(399, argv[1], &gps) != 0) {
-        printf("get gps failed?\n");
+        printf("get gps failed for %s - check if file exists\n", argv[1]);
+        return -1;
     }
-    else {
-        printf("gps:  (%d.%d , %d.%d)\n",
-                gps.lat_integer,
-                gps.lat_fractional,
-                gps.lng_integer,
-                gps.lng_fractional);
-    }
+
+    printf("%s location info\n"
+            "latitude\t\tlongitude\t\taccuracy(m)\n"
+            "%d.%d\t\t%d.%d\t\t%d\n",
+            argv[1],
+            gps.lat_integer, gps.lat_fractional,
+            gps.lng_integer, gps.lng_fractional,
+            gps.accuracy);
+
+    printf("google maps link: http://www.google.com/maps/place/%d.%d, %d.%d",
+            gps.lat_integer, gps.lat_fractional,
+            gps.lng_integer, gps.lng_fractional);
+
     return 0;
 }
